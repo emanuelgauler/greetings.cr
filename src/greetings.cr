5b6d28f5e861692aa8df284e81cb51c1e077775c
emanuelgauler <emanuelgauler@gmail.com>
2026-04-22 08:42:31 -0300

    Armé la ventana principal

--- src/greetings.cr
+++ src/greetings.cr
@@ -1,6 +1,31 @@
-# TODO: Write documentation for `Greetings`
-module Greetings
-  VERSION = "0.1.0"
+# guardar como app.cr
+require "uing"
 
-  # TODO: Put your code here
+UIng.init do
+
+	 window = UIng::Window.new("Saludo", width: 320, height: 140) do |w|
+			w.on_closing { UIng.quit; true }
+
+			# Etiqueta
+			label = UIng::Label.new("Su nombre aquí")
+
+			# Campo de texto
+			input = UIng::Entry.new
+
+			# Botón enviar
+			button = UIng::Button.new("Enviar")
+			button.on_clicked do
+				 nombre = input.text || "mundo"
+				 w.msg_box("Saludo", "Hola, #{nombre}!")
+			end
+
+			vbox = UIng::Box.new(:vertical, padded: true)
+			vbox.append(label)
+			vbox.append(input)
+			vbox.append(button)
+			w.child = vbox
+			w.show
+	 end
+	 UIng.main
 end
+
